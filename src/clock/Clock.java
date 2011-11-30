fcbb8649260f825e477cf4fb95674e4a25a16671
Robert Vonz <rdvonz@gmail.com>
2011-11-29 16:32:14 -0800

    Triumph! Clock is now displayed! Added Font Resources (Fonts are made in a program called Hiero)

--- src/clock/Clock.java
+++ src/clock/Clock.java
@@ -17,17 +17,18 @@ import org.newdawn.slick.AngelCodeFont;
 public class Clock extends BasicGame{
 	DateFormat dateFormat;
 	Calendar cal;
+	AngelCodeFont font;
+	String clockText = "test";
 	public Clock(){
 		/* Constructor */
-		super("Avoid the blocks!");
+		super("Clock");
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException{
+		font = new AngelCodeFont("resources/newfont.fnt", "resources/newfont.png");
 		 dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		//get current date time with Date()
-		Date date = new Date();
-		System.out.println(dateFormat.format(date));
 
 		//get current date time with Calendar()
 
@@ -35,12 +36,13 @@ public class Clock extends BasicGame{
 
 	public void update(GameContainer gc, int delta) throws SlickException{
 		cal = Calendar.getInstance();
-		System.out.println(dateFormat.format(cal.getTime()));
+		clockText = (dateFormat.format(cal.getTime())).toString();
 
 
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException{
+		font.drawString(50, 50, clockText);
 	}
 
 	public static void main(String[] args) throws SlickException{
