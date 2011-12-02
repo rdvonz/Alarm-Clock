39c9c7b09909873de083997241c6d4cdd6381f0f
Robert Vonz <rdvonz@gmail.com>
2011-12-02 11:53:44 -0800

    Modified class to use with Alarm Clock

--- src/Mp3Player.java
+++ src/Mp3Player.java
@@ -2,6 +2,7 @@ import javazoom.jl.decoder.Decoder;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -11,24 +12,20 @@ import javax.swing.JFileChooser;
 
 public class Mp3Player
 {
-	private String filename;
 	private static Player player;
 	private static FileInputStream file;
 	private static BufferedInputStream bis;
-	
-	public static void main(String[] args) throws FileNotFoundException, JavaLayerException{
-		JFileChooser fc = new JFileChooser();
-		fc.showOpenDialog(null);
-		file = new FileInputStream(fc.getSelectedFile());
-		bis = new BufferedInputStream(file);
-		
-		player = new Player(bis);
-		
-		String path = fc.getSelectedFile().getPath();
-		System.out.println(path);
-		
-		player.play();
-		
-	}
+	static int ERROR;
 
+	public Mp3Player(File filename){
+		try{
+			file = new FileInputStream(filename);
+			bis = new BufferedInputStream(file);
+			player = new Player(bis);
+			player.play();
+		}
+		catch(Exception e){
+			ERROR = -1;
+		}
+	}
 }
