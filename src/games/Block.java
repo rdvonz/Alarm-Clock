766adf0b6973210c10fdd60594c8883edd35010e
Robert Vonz <rdvonz@gmail.com>
2011-12-02 11:26:12 -0800

    Changed x and y values

--- src/games/Block.java
+++ src/games/Block.java
@@ -17,8 +17,8 @@ public class Block extends Rectangle{
 		super(50, 50, 50, 50);
 		xVel = r.nextInt(5);
 		yVel = r.nextInt(5);
-		x = r.nextInt(10);
-		y = r.nextInt(10);
+		x = r.nextInt(640);
+		y = r.nextInt(480);
 	}
 public void setLoc(){
 		super.setLocation(x+=xVel, y+=yVel);
