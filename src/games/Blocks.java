0e49d0124192a326afbc6850e43ec07684e2770f
Robert Vonz <rdvonz@gmail.com>
2011-12-02 11:10:45 -0800

    Class that creates an arry list of blocks for easy updating

---
+++ src/games/Blocks.java
@@ -0,0 +1,37 @@
+package games;
+                              
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.newdawn.slick.geom.Rectangle;
+public class Blocks{
+	Rectangle rect;
+	ArrayList<Block> blocks;
+	Random r = new Random();
+	Block blck;
+	int xVel;
+	int yVel;
+	int x;
+	int y;
+
+	public Blocks(int num){
+		blocks = new ArrayList<Block>();
+		for(int i=0; i<num; i++){
+			blocks.add(new Block());
+		}
+		}
+		
+	public Rectangle getBlock(int numBlock){
+		return blocks.get(numBlock);
+	}
+	public int getNumBlocks(){
+		return blocks.size();
+	}
+	
+	public void update(){
+		for(int i=0; i<getNumBlocks(); i++){
+			((Block) getBlock(i)).setLoc();
+		}
+	}
+	
+}
