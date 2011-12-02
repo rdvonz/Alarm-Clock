fa8b7185f09c0b12bcda979e8d7fbb97326e6ead
Robert Vonz <rdvonz@gmail.com>
2011-12-02 11:08:50 -0800

    Revert "Class that creates an array list of blocks (for the avoid game)"
    This reverts commit 3add68f22316db832b48b608d9503f991b7c2fc8.

--- src/games/Block.java
+++ src/games/Block.java
@@ -1,29 +1,23 @@
 package games;
 
-import java.util.ArrayList;
 import java.util.Random;
-
-import org.newdawn.slick.geom.Rectangle;
-public class Block extends Rectangle{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	ArrayList<Rectangle> rects;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+public class Block extends Image{
+	Image sprite = null;
 	Random r = new Random();
 	int xVel;
 	int yVel;
 	int x;
 	int y;
-
-	public Block(){
-		super(50, 50, 50, 50);
-		xVel = r.nextInt(5);
-		yVel = r.nextInt(5);
+	
+	Block() throws SlickException{
+		sprite = new Image("resrouces/plane.png");
+		xVel = r.nextInt(10);
+		yVel = r.nextInt(10);
+		
 		x = r.nextInt(640);
 		y = r.nextInt(480);
 	}
-public void setLoc(){
-		super.setLocation(x+=xVel, y+=yVel);
-	}
+
 }
